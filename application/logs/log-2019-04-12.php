8cf07f2cf887a50e285ebb9fcbe5b823b6ee5097
JenitLei <jenitunique@gmail.com>
2019-04-25 16:48:20 +0530

    all code

---
+++ application/logs/log-2019-04-12.php
@@ -0,0 +1,123 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-04-12 13:49:05 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No such file or directory /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2019-04-12 13:49:05 --> Unable to connect to the database
+ERROR - 2019-04-12 13:49:05 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
+ERROR - 2019-04-12 13:49:55 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No such file or directory /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2019-04-12 13:49:55 --> Unable to connect to the database
+ERROR - 2019-04-12 13:49:55 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
+ERROR - 2019-04-12 13:49:56 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No such file or directory /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2019-04-12 13:49:56 --> Unable to connect to the database
+ERROR - 2019-04-12 13:49:56 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
+ERROR - 2019-04-12 13:50:47 --> Severity: Warning --> mkdir(): Invalid path /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/libraries/Session/drivers/Session_files_driver.php 136
+ERROR - 2019-04-12 13:50:47 --> Severity: error --> Exception: Session: Configured save path '' is not a directory, doesn't exist or cannot be created. /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/libraries/Session/drivers/Session_files_driver.php 138
+ERROR - 2019-04-12 13:50:47 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
+ERROR - 2019-04-12 13:52:34 --> Severity: Warning --> mkdir(): Invalid path /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/libraries/Session/drivers/Session_files_driver.php 136
+ERROR - 2019-04-12 13:52:34 --> Severity: error --> Exception: Session: Configured save path '' is not a directory, doesn't exist or cannot be created. /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/libraries/Session/drivers/Session_files_driver.php 138
+ERROR - 2019-04-12 13:52:34 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
+ERROR - 2019-04-12 13:52:53 --> Severity: Warning --> mkdir(): Invalid path /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/libraries/Session/drivers/Session_files_driver.php 136
+ERROR - 2019-04-12 13:52:53 --> Severity: error --> Exception: Session: Configured save path '' is not a directory, doesn't exist or cannot be created. /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/libraries/Session/drivers/Session_files_driver.php 138
+ERROR - 2019-04-12 13:52:53 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
+ERROR - 2019-04-12 13:52:55 --> Severity: Warning --> mkdir(): Invalid path /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/libraries/Session/drivers/Session_files_driver.php 136
+ERROR - 2019-04-12 13:52:55 --> Severity: error --> Exception: Session: Configured save path '' is not a directory, doesn't exist or cannot be created. /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/libraries/Session/drivers/Session_files_driver.php 138
+ERROR - 2019-04-12 13:52:55 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
+ERROR - 2019-04-12 13:55:36 --> Severity: Warning --> mkdir(): Invalid path /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/libraries/Session/drivers/Session_files_driver.php 136
+ERROR - 2019-04-12 13:55:36 --> Severity: error --> Exception: Session: Configured save path '' is not a directory, doesn't exist or cannot be created. /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/libraries/Session/drivers/Session_files_driver.php 138
+ERROR - 2019-04-12 13:55:36 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
+ERROR - 2019-04-12 14:06:39 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:06:39 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:06:39 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:06:39 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:06:48 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:06:49 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:06:50 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:06:51 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:06:55 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:07:12 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:07:16 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:07:47 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:07:47 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:07:48 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:17 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:17 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:17 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:17 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:18 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:19 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:19 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:19 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:20 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:20 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:20 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:20 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:24 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:24 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:24 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:24 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:34 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:34 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:37 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:37 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:49 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:49 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:53 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:08:53 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:09:33 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:09:33 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:09:59 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:09:59 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:10:40 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:10:40 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:10:59 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:10:59 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:11:06 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:11:07 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:11:07 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:11:07 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:11:59 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:11:59 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:11:59 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:11:59 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:03 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:03 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:03 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:03 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:09 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:09 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:09 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:09 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:25 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:25 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:25 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:25 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:34 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:34 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:35 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:35 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:41 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:42 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:42 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:12:42 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:16:51 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:16:51 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:16:51 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:18:04 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:18:04 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:18:10 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:18:10 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:19:26 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:19:26 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:19:36 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 14:19:37 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 15:03:04 --> Image Upload Error: <p>You did not select a file to upload.</p>
+ERROR - 2019-04-12 15:05:36 --> Image Upload Error: <p>You did not select a file to upload.</p>
+ERROR - 2019-04-12 15:43:21 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'eCom' /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2019-04-12 15:43:21 --> Unable to connect to the database
+ERROR - 2019-04-12 15:43:21 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
+ERROR - 2019-04-12 15:43:29 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'eCom' /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2019-04-12 15:43:29 --> Unable to connect to the database
+ERROR - 2019-04-12 15:43:29 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
+ERROR - 2019-04-12 15:48:24 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 15:48:24 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 15:48:29 --> 404 Page Not Found: /index
+ERROR - 2019-04-12 15:48:30 --> 404 Page Not Found: /index
---
+++ application/logs/log-2019-04-25.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-04-25 16:04:15 --> Query error: Table 'eCom.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2019-04-25 16:05:16 --> Query error: Table 'eCom.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2019-04-25 16:05:18 --> Query error: Table 'eCom.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2019-04-25 16:05:18 --> Query error: Table 'eCom.value_store' doesn't exist - Invalid query: SELECT value FROM value_store WHERE thekey = 'outOfStock'
+ERROR - 2019-04-25 16:06:15 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'eCom' /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2019-04-25 16:06:15 --> Unable to connect to the database
+ERROR - 2019-04-25 16:06:15 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
+ERROR - 2019-04-25 16:10:51 --> Severity: Warning --> mkdir(): Invalid path /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/libraries/Session/drivers/Session_files_driver.php 136
+ERROR - 2019-04-25 16:10:51 --> Severity: error --> Exception: Session: Configured save path '' is not a directory, doesn't exist or cannot be created. /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/libraries/Session/drivers/Session_files_driver.php 138
+ERROR - 2019-04-25 16:10:51 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
+ERROR - 2019-04-25 16:14:35 --> Severity: Notice --> Undefined property: CI::$CI /var/www/html/Ecommerce-CodeIgniter-Bootstrap/application/third_party/MX/Controller.php 59
+ERROR - 2019-04-25 16:14:35 --> Severity: Notice --> Trying to get property of non-object /var/www/html/Ecommerce-CodeIgniter-Bootstrap/application/controllers/Home.php 20
+ERROR - 2019-04-25 16:14:35 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
+ERROR - 2019-04-25 16:14:35 --> Severity: Error --> Call to a member function getSeo() on null /var/www/html/Ecommerce-CodeIgniter-Bootstrap/application/controllers/Home.php 20
+ERROR - 2019-04-25 16:16:34 --> Severity: Notice --> Undefined property: CI::$CI /var/www/html/Ecommerce-CodeIgniter-Bootstrap/application/third_party/MX/Controller.php 59
+ERROR - 2019-04-25 16:16:34 --> Severity: Notice --> Trying to get property of non-object /var/www/html/Ecommerce-CodeIgniter-Bootstrap/application/controllers/Home.php 20
+ERROR - 2019-04-25 16:16:34 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
+ERROR - 2019-04-25 16:16:34 --> Severity: Error --> Call to a member function getSeo() on null /var/www/html/Ecommerce-CodeIgniter-Bootstrap/application/controllers/Home.php 20
+ERROR - 2019-04-25 16:17:16 --> Severity: Warning --> mkdir(): Invalid path /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/libraries/Session/drivers/Session_files_driver.php 136
+ERROR - 2019-04-25 16:17:16 --> Severity: error --> Exception: Session: Configured save path '' is not a directory, doesn't exist or cannot be created. /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/libraries/Session/drivers/Session_files_driver.php 138
+ERROR - 2019-04-25 16:17:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Exceptions.php:271) /var/www/html/Ecommerce-CodeIgniter-Bootstrap/system/core/Common.php 570
